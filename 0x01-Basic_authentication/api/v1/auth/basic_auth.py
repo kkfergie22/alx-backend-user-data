58fd2d6b3c557b4039bf4171be29c435f53a0c16
Christian Koranteng <korantengchristian@gmail.com>
2023-05-02 14:17:03 +0000

    Upload 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module of Basic Authentication"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+
+
+class BasicAuth(Auth):
+    pass
