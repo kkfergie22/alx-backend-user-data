008d9a7837df56e960e731dff77f4a5dd2f1f050
Christian Koranteng <korantengchristian@gmail.com>
2023-05-02 14:24:27 +0000

    Upload 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,19 @@ from typing import TypeVar
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract base64 authorization header
+        Args:
+            authorization_header (str): authorization header
+        Returns:
+            str: base64 part of the authorization header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
