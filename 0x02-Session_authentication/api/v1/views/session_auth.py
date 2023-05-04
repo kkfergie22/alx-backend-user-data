a3cb631c8930e7a92990fbeaae4dedf1a147f4fa
Christian Koranteng <korantengchristian@gmail.com>
2023-05-04 14:12:16 +0000

    Update 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Session authentication module"""
 import os
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 from api.v1.views import app_views
 from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
+def session_login() -> str:
     """ POST /api/v1/auth_session/login"""
     email = request.form.get('email')
     password = request.form.get('password')
@@ -20,10 +20,11 @@ def login() -> str:
         return jsonify({"error": "no user found for this email"}), 404
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    else:
-        from api.v1.app import auth
-        session_id = auth.create_session(user.id)
-        cookie_name = os.getenv('SESSION_NAME')
-        response = jsonify(user.to_json())
-        response.set_cookie(cookie_name, session_id)
-        return response
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    user_json = user.to_json()
+    response = make_response(jsonify(user_json), 200)
+    cookie_name = os.getenv('SESSION_NAME')
+    response.set_cookie(cookie_name, session_id)
+
+    return response
