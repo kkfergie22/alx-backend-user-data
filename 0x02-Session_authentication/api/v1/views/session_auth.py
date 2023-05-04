cc6ef5d88b5022b740da7fede6a814a6838062fa
Christian Koranteng <korantengchristian@gmail.com>
2023-05-04 14:20:14 +0000

    Upload 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Session authentication module"""
 import os
-from flask import jsonify, request, make_response
+from flask import jsonify, request, make_response, abort
 from api.v1.views import app_views
 from models.user import User
 
@@ -20,11 +20,23 @@ def session_login() -> str:
         return jsonify({"error": "no user found for this email"}), 404
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    user_json = user.to_json()
-    response = make_response(jsonify(user_json), 200)
-    cookie_name = os.getenv('SESSION_NAME')
-    response.set_cookie(cookie_name, session_id)
+    else:
+        from api.v1.app import auth
+        session_id = auth.create_session(user.id)
+        user_json = user.to_json()
+        response = make_response(jsonify(user_json), 200)
+        cookie_name = os.getenv('SESSION_NAME')
+        response.set_cookie(cookie_name, session_id)
 
-    return response
+        return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def session_logout() -> str:
+    """ DELETE /api/v1/auth_session/logout"""
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    else:
+        return jsonify({}), 200
