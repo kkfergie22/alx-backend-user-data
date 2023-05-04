ff92fbd4da587dd5ffa005df953d999034bf32cc
Christian Koranteng <korantengchristian@gmail.com>
2023-05-04 12:52:23 +0000

    Upload fix to app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,9 +56,8 @@ def before_request_func() -> str:
                       '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.session_cookie(request) is None:
-        abort(401)
-    if auth.authorization_header(request) is None:
+    if auth.session_cookie(request) is None and auth.authorization_header(
+            request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
