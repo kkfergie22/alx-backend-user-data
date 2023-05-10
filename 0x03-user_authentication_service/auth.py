944621970d56b1f608eeaf8fd89b8e5a8acaf713
Christian Koranteng <korantengchristian@gmail.com>
2023-05-10 12:45:35 +0000

    Update valid_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,6 +36,7 @@ class Auth:
 
     def valid_login(self, email: str, password: str) -> bool:
         """ Check if the login is valid """
+        user = None
         try:
             user = self._db.find_user_by(email=email)
             hashed_password = user.hashed_password.encode('utf-8')
