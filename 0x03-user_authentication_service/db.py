a57a4bf82bcd4da2d0e4e96d605b15bc6ce83846
Christian Koranteng <korantengchristian@gmail.com>
2023-05-10 09:15:56 +0000

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -34,11 +36,11 @@ class DB:
         self._session.commit()
         return new_user
 
-    # def find_user_by(self, **kwargs) -> User:
-    #     """Find a user by a specific attribute"""
-    #     if not kwargs:
-    #         raise InvalidRequestError
-    #     result = self._session.query(User).filter_by(**kwargs).first()
-    #     if result is None:
-    #         raise NoResultFound
-    #     return result
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a specific attribute"""
+        if not kwargs:
+            raise InvalidRequestError
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if result is None:
+            raise NoResultFound
+        return result
