507c30d1529971db844e0608b0f761a28050419d
Christian Koranteng <korantengchristian@gmail.com>
2023-05-11 18:43:54 +0000

    Swap positions of reset and passwords

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,7 +86,7 @@ class Auth:
         except NoResultFound:
             raise ValueError("No result found")
 
-    def update_password(self, password: str, reset_token: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """ Update a user's password """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
