f7d8b38ca7af9e95738b407179176bc3ab9c170d
Christian Koranteng <korantengchristian@gmail.com>
2023-05-11 16:37:51 +0000

    Fix type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,7 +45,7 @@ class DB:
             raise NoResultFound
         return result
 
-    def update_user(self, user_id, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """Update a user"""
         if not kwargs:
             raise ValueError("No attributes to update were provided")
