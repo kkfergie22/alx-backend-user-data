bb2c14e37ea066842f27d0f87dc906a96fcee5db
Christian Koranteng <korantengchristian@gmail.com>
2023-05-13 15:13:24 +0000

    Upload file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,8 +9,6 @@ import mysql.connector
 from typing import List
 
 
-
-
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Returns the log message obfuscated.
