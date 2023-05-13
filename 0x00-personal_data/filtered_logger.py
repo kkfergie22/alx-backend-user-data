efd1348fffd32db6cbb5276e62ffbb72ccae5d69
Christian Koranteng <korantengchristian@gmail.com>
2023-05-13 16:05:19 +0000

    Fix the documentation bug

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Regex-ing and obfuscating personal data fields"""
+
+"""Module for obfuscating personal data fields"""
 
 import logging
 import re
@@ -26,72 +27,72 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+# class RedactingFormatter(logging.Formatter):
+#     """ Redacting Formatter class """
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+#     REDACTION = "***"
+#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+#     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str] = None):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+#     def __init__(self, fields: List[str] = None):
+#         super(RedactingFormatter, self).__init__(self.FORMAT)
+#         self.fields = fields
 
-    def format(self, record: logging.LogRecord) -> str:
-        message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message,
-                            self.SEPARATOR)
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+#     def format(self, record: logging.LogRecord) -> str:
+#         message = super().format(record)
+#         return filter_datum(self.fields, self.REDACTION, message,
+#                             self.SEPARATOR)
 
 
-def get_logger() -> logging.Logger:
-    """Returns a logger object."""
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
-
-    formatter = RedactingFormatter(PII_FIELDS)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-    logger.propagate = False
-
-    return logger
+# PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to the secure holberton database."""
-    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    database = os.environ.get('PERSONAL_DATA_DB_NAME')
+# def get_logger() -> logging.Logger:
+#     """Returns a logger object."""
+#     logger = logging.getLogger('user_data')
+#     logger.setLevel(logging.INFO)
 
-    conn = mysql.connector.connect(user=username, password=password,
-                                   host=host, database=database)
-    return conn
+#     formatter = RedactingFormatter(PII_FIELDS)
+#     handler = logging.StreamHandler(sys.stdout)
+#     handler.setFormatter(formatter)
 
+#     logger.addHandler(handler)
+#     logger.propagate = False
 
-def main():
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    handler = logging.StreamHandler()
-    formatter = RedactingFormatter(PII_FIELDS)
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    query = "SELECT * FROM users"
-    cursor.execute(query)
-    for row in cursor:
-        message = '; '.join([f"{k}={v}" for k, v in row.items()])
-        logger.info(message)
+#     return logger
 
 
-if __name__ == '__main__':
-    main()
+# def get_db() -> mysql.connector.connection.MySQLConnection:
+#     """Returns a connector to the secure holberton database."""
+#     username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+#     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+#     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+#     database = os.environ.get('PERSONAL_DATA_DB_NAME')
+
+#     conn = mysql.connector.connect(user=username, password=password,
+#                                    host=host, database=database)
+#     return conn
+
+
+# def main():
+#     logger = logging.getLogger('user_data')
+#     logger.setLevel(logging.INFO)
+#     logger.propagate = False
+
+#     handler = logging.StreamHandler()
+#     formatter = RedactingFormatter(PII_FIELDS)
+#     handler.setFormatter(formatter)
+
+#     logger.addHandler(handler)
+
+#     db = get_db()
+#     cursor = db.cursor(dictionary=True)
+#     query = "SELECT * FROM users"
+#     cursor.execute(query)
+#     for row in cursor:
+#         message = '; '.join([f"{k}={v}" for k, v in row.items()])
+#         logger.info(message)
+
+
+# if __name__ == '__main__':
+#     main()
